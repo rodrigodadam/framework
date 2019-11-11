4c76d15240bd7307ea4c4587bda2073e9c035f89
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-11 21:15:21 +0000

    add composer autoloading

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,10 +5,5 @@ $app = [];
 $app['config'] = require 'config.php';
 
 
-
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connector.php';
-require 'core/database/QueryBuilder.php';
-
-$app['database'] = new QueryBuilder(Connector::make($app['config']['database']));
+$app['database'] = new QueryBuilder(
+    Connector::make($app['config']['database']));
